6d488d570a479d5973f720a2b6b768679d4a28ce
sunghyun9737 <sunghyun9737@gmail.com>
2026-01-05 10:34:03 +0900

    2026/01/05 BootLastProject Start

---
+++ src/main/java/com/sist/web/vo/SeoulVO.java
@@ -0,0 +1,22 @@
+package com.sist.web.vo;
+
+import lombok.Data;
+
+/*
+ * NO          NOT NULL NUMBER         
+TITLE                VARCHAR2(1024) 
+IMAGE1               VARCHAR2(1024) 
+IMAGE2               VARCHAR2(1024) 
+X                    NUMBER(20,12)  위도
+Y                    NUMBER(20,12)  경도
+CONTENTID            NUMBER         상세보기
+ADDRESS              VARCHAR2(300)  
+CONTENTTYPE          NUMBER(2)      카테고리
+HIT                  NUMBER         
+ */
+@Data
+public class SeoulVO {
+	private int no, contentid, contenttype,hit;
+	private double x,y;
+	private String title,image1,image2,address;
+}
